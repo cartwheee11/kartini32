49557cee64447db8676f5c3c07ae5986c0458a38
cartwheee11 <beardedstump@yandex.ru>
2025-03-10 15:34:04 +0300

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,428 @@
+<template>
+  <main>
+    <nav class="has-background-black-bis" style="position: fixed; width: 100%; top: 0; z-index: 200">
+      <div class="inner-nav content" style="padding: 15px 0; margin-bottom: 0">
+        <h3 class="logo" style="margin: 0; position: absolute; right: 20px">8 (903) 819 39 09</h3>
+        <h3 style="margin: 0">картины</h3>
+      </div>
+
+      <hr style="padding: 0; margin: 0" />
+    </nav>
+
+    <div class="header-container-wrapper">
+      <div class="header-images">
+        <img src="../assets/header-images/header-image1.jpg" width="100" alt="" />
+        <img src="../assets/header-images/header-image2.jpg" width="100" alt="" />
+        <img src="../assets/header-images/header-image3.jpg" width="100" alt="" />
+        <img src="../assets/header-images/header-image4.jpg" width="100" alt="" />
+        <img src="../assets/header-images/header-image5.jpg" width="100" alt="" />
+        <img src="../assets/header-images/header-image6.jpg" width="100" alt="" />
+        <img src="../assets/header-images/header-image7.jpg" width="100" alt="" />
+        <img src="../assets/header-images/header-image8.jpg" width="100" alt="" />
+        <img src="../assets/header-images/header-image9.jpg" width="100" alt="" />
+      </div>
+
+      <section class="container has-text-centered content is-large header-container">
+        <h1 class="" style="margin-top: 57px">
+          Доступные <br />
+          картины в Брянске
+        </h1>
+        <a href="tel:+79038193909"><button class="button is-primary is-large is-responsive">Позвонить</button></a>
+      </section>
+    </div>
+
+    <hr style="padding: 0; margin: 0" />
+    <section class="map-section">
+      <div class="container">
+        <div class="content is-medium">
+          <div class="columns is-desktop">
+            <div class="column">
+              <h1>Как нас найти?</h1>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2387.8130800058975!2d34.354695961653356!3d53.239126934286816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x412d58f8018b4617%3A0x9d894b4f3f2b0a90!2z0YPQuy4g0JXQvNC70Y7RgtC40L3QsCwgNDIsINCR0YDRj9C90YHQuiwg0JHRgNGP0L3RgdC60LDRjyDQvtCx0LsuLCAyNDEwNTA!5e0!3m2!1sru!2sru!4v1589579621495!5m2!1sru!2sru"
+                height="600" frameborder="0" style="border: 0; width: 100%" allowfullscreen="" aria-hidden="false"
+                tabindex="0">
+              </iframe>
+            </div>
+            <div class="column">
+              <div class="content">
+                <h1>Наш адрес</h1>
+                <p>
+                  <code>тц «Брянский»:</code> г. Брянск, Советский район, центральный рынок,
+                  остановка «Строительный техникум», ул. Емлютина. д. 42, 1 этаж
+                </p>
+
+                <div>
+                  <table class="is-small" style="width: 100%">
+                    <thead>
+                      <tr>
+                        <th>День</th>
+                        <th>Время</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Понедельник</td>
+                        <td>Выходной</td>
+                      </tr>
+                      <tr>
+                        <td>Вторник</td>
+                        <td>10:00 — 17:00</td>
+                      </tr>
+                      <tr>
+                        <td>Среда</td>
+                        <td>10:00 — 17:00</td>
+                      </tr>
+                      <tr>
+                        <td>Четверг</td>
+                        <td>10:00 — 17:00</td>
+                      </tr>
+                      <tr>
+                        <td>Пятница</td>
+                        <td>10:00 — 17:00</td>
+                      </tr>
+                      <tr>
+                        <td>Суббота</td>
+                        <td>10:00 — 17:00</td>
+                      </tr>
+                      <tr>
+                        <td>Воскресенье</td>
+                        <td>10:00 — 17:00</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <hr style="padding: 0; margin: 0" />
+    <section>
+      <div class="content container">
+        <h1 style="font-size: 80px">
+          <span class="has-background-white has-text-black-bis">В наличии имеется более 1000 картин.</span>
+          Консультант подберет картину специально под ваш интерьер и бюджет. Также вы можете
+          заказать у нас любой сюжет
+        </h1>
+        <a href="tel:+79038193909"><button class="button is-primary is-large is-responsive">Позвонить</button></a>
+      </div>
+    </section>
+
+    <hr style="padding: 0; margin: 0" />
+
+    <section class="svetlana-section">
+      <div class="container content is-large">
+        <div class="columns">
+          <div class="column is-three-fifths">
+            <img style="width: 100%" src="../assets/images/svetlana.jpg" alt="" class="is-fluid" />
+          </div>
+          <div class="column">
+            <div class="content">
+              <h1>Головко Светлана — Художник</h1>
+              <p>
+                Объединила вокруг себя коллектив лучших художников Брянска, чтобы предложить вам
+                огромный выбор картин
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <hr style="padding: 0; margin: 0" />
+
+    <section>
+      <div class="container">
+        <div class="swiper">
+          <!-- Additional required wrapper -->
+          <div class="swiper-wrapper">
+            <!-- Slides -->
+            <div class="swiper-slide">
+              <img src="../assets/images/swiper-images/1.jpeg" alt="" />
+            </div>
+            <div class="swiper-slide">
+              <img src="../assets/images/swiper-images/2.jpeg" alt="" />
+            </div>
+            <div class="swiper-slide">
+              <img src="../assets/images/swiper-images/3.jpeg" alt="" />
+            </div>
+          </div>
+          <!-- If we need pagination -->
+          <div class="swiper-pagination"></div>
+
+          <!-- If we need navigation buttons -->
+          <div class="swiper-button-prev"></div>
+          <div class="swiper-button-next"></div>
+
+          <!-- If we need scrollbar -->
+          <div class="swiper-scrollbar"></div>
+        </div>
+      </div>
+    </section>
+
+    <hr style="padding: 0; margin: 0" />
+    <section class="feedback-section">
+      <div class="container">
+        <div class="content is-large">
+          <h1>наши клиенты</h1>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <div v-for="rev in reviews" :key="rev.text" class="card has-background-black-ter">
+              <div class="card-content">
+                <div class="media" style="align-items: center">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img style="border-radius: 100%" :src="rev.avatar" alt="" />
+                    </figure>
+                  </div>
+
+                  <div class="media-content">
+                    <div class="content">
+                      <p class="title is-4">{{ rev.name }}</p>
+                    </div>
+                  </div>
+                  <img src="../assets/images/stars.png" alt="" />
+                </div>
+
+                <hr />
+                <div class="content is-large is-responsive">{{ rev.text }}</div>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div v-for="rev in reviews2" :key="rev.text" class="card has-background-black-ter">
+              <div class="card-content">
+                <div class="media" style="align-items: center">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img style="border-radius: 100%" :src="rev.avatar" alt="" />
+                    </figure>
+                  </div>
+
+                  <div class="media-content">
+                    <div class="content">
+                      <p class="title is-4">{{ rev.name }}</p>
+                    </div>
+                  </div>
+
+                  <img src="../assets/images/stars.png" alt="" />
+                </div>
+
+                <hr />
+                <div class="content is-large is-responsive">{{ rev.text }}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <hr style="padding: 0; margin: 0" />
+
+    <section>
+      <div class="container content has-text-centered">
+        <h1 class="title" style="font-size: 120px">8(903)819-39-09</h1>
+        <p class="subtitle">Позвоните по этому номеру и получите скидку 10%</p>
+        <a href="tel:+79038193909"><button class="button is-primary is-large is-responsive">Позвонить</button></a>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+// import WOW from "../assets/lib/wow.min.js"
+
+import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
+// import Swiper and modules styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+
+import revs from '../assets/reviews.json'
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      images: [],
+      vel: [],
+
+      reviews: [],
+      reviews2: [],
+    }
+  },
+
+  mounted() {
+    new Swiper('.swiper', {
+      // configure Swiper to use modules
+      modules: [Navigation, Pagination],
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      loop: true,
+
+      slidesPerView: 1.2,
+      spaceBetween: 20,
+      breakpoints: {
+        1000: {
+          slidesPerView: 2.1,
+        },
+      },
+    })
+
+    const allRevs = revs
+    allRevs.forEach((rev, id) => {
+      if (id < 3) {
+        this.reviews.push(rev)
+      } else {
+        this.reviews2.push(rev)
+      }
+    })
+
+    const imgs = document.querySelectorAll('.header-container-wrapper img')
+    this.images = imgs
+
+      ;[].forEach.call(imgs, (img) => {
+        this.vel.push(Math.random() * 3 + 1)
+        img.style = `
+          // transform: rotate(${Math.random() * 180}deg);
+          top: ${Math.random() * window.innerHeight - 50}px;
+          left: ${Math.random() * window.innerWidth - 100}px;
+        `
+      })
+
+    this.startUpdating()
+
+    console.log(this.images)
+  },
+
+  methods: {
+    startUpdating() {
+      let update = () => {
+        ;[].forEach.call(this.images, (img, i) => {
+          img.style.top =
+            parseInt(img.style.top) > window.innerHeight
+              ? -100 + 'px'
+              : parseInt(img.style.top) + this.vel[i] + 'px'
+        })
+
+        setTimeout(update.bind(this), 16)
+      }
+
+      update()
+    },
+  },
+}
+</script>
+
+<style>
+.header-container-wrapper img {
+  top: 0;
+  position: absolute;
+  z-index: -1;
+}
+
+.swiper-slide img {
+  border-radius: 10px;
+}
+
+.swiper-button-next,
+.swiper-button-prev {
+  width: 70px;
+  border-radius: 10px;
+  background-color: #2e333d;
+}
+
+.swiper-button-next::after,
+.swiper-button-prev::after {
+  color: white;
+  font-size: 20px;
+  font-weight: 800;
+}
+
+.card {
+  /* margin-bottom: 60px; */
+  box-shadow: none;
+}
+
+.card hr {
+  background-color: #272b34;
+}
+
+h1,
+h3,
+h2 {
+  font-family: bebas;
+}
+
+.header-images {
+  overflow-y: hidden !important;
+  overflow: hidden !important;
+
+  z-index: 0;
+}
+
+section {
+  padding: 100px 0;
+}
+
+.map-section {
+  background-color: #14161a;
+  z-index: 2;
+  position: relative;
+}
+
+.header-container-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  justify-items: center;
+  height: 90vh;
+  overflow-y: hidden !important;
+  overflow: hidden !important;
+}
+
+.header-container {
+  flex-grow: 0 !important;
+}
+
+.header-container h1 {
+  font-size: 140px;
+  text-transform: uppercase;
+  font-family: bebas;
+}
+
+nav {
+  text-align: center;
+}
+
+@media (max-width: 1000px) {
+  nav {
+    text-align: left;
+  }
+
+  .inner-nav {
+    padding-left: 20px !important;
+  }
+
+  .header-container-wrapper {
+    height: 60vh;
+  }
+
+  section {
+    padding: 30px 15px;
+  }
+
+  h1 {
+    font-size: 40px !important;
+  }
+
+  .card {
+    margin: 0;
+  }
+}
+</style>
